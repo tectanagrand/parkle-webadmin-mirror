8f2a9128cdc2702ed7e1f8669ce24d8f27a3d5f2
tectanagrand <rtektano@gmail.com>
2024-04-23 08:49:35 +0700

    fix : mock data parking status

--- mock/ParkingStatus.js
+++ mock/ParkingStatus.js
@@ -1,26 +1,26 @@
-const ParkingStatus = [
+export const ParkingStatus = [
   {
     parking_id: "#1312",
     customers: "Habibi Abiyyu",
     time: "12/12/2023 - 17:56 WIB",
-    status: "filled",
+    status: "Parking Filled",
   },
   {
     parking_id: "#1314",
     customers: "John Doe",
     time: "12/12/2023 - 17:56 WIB",
-    status: "booking",
+    status: "Booking",
   },
   {
     parking_id: "#1314",
     customers: "Kendrick Lamar",
     time: "12/12/2023 - 17:56 WIB",
-    status: "empty",
+    status: "Empty",
   },
   {
     parking_id: "#1314",
     customers: "Kahli Uchi",
     time: "12/12/2023 - 17:56 WIB",
-    status: "empty",
+    status: "Empty",
   },
 ];
